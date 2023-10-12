9dd308f79b2dbe931a69ff2397f5c9daa687ba5a
Kavidu <kaviduwarnakulasuriya@gmail.com>
2023-10-12 23:53:10 +0200

    S3G4 rifatto

--- script.js
+++ script.js
@@ -11,29 +11,56 @@
 // Mantieni evidenziate le celle precedentemente estratti dopo la nuovs estrazione
 //extra
 
-// for( let i = 0 ; i < 76 ; i++){
+const createTable = () => {
+  const table = document.getElementById("table");
+  for (let i = 0; i < 76; i++) {
+    const cellNumber = document.createElement("div");
+    cellNumber.classList.add("cell");
 
-//     const cella = document.createElement("div");
-//     cella.classList.add("tabella");
-//     const numero = document.createElement("H3");
-//     giorno.innerText = i;
-//     console.log()
-// }
+    const cellContent = document.createElement("h3");
+    cellContent.innerText = i + 1;
+    //cellContent.appendChild(cellNumber);
+    cellNumber.appendChild(cellContent);
+    table.appendChild(cellNumber);
+  }
+};
 
-for (let i = 0; i < 76; i++) {
-  const cella = document.createElement("div");
-  cella.classList.add("tabella");
+window.onload = () => {
+  createTable();
+  bottone.addEventListener("click", function () {
+    function1();
+  });
+  generateNewSchedule.addEventListener("click", function () {
+    generateTable();
+  });
+};
 
-  const numeroCella = document.createElement("h3");
-  numeroCella.innerText = i + 1;
-  console.log(numeroCella);
+const function1 = function () {
+  const random = Math.floor(Math.random() * 76);
+  console.log(random);
+  const example = document.getElementById("randomNumber");
+  example.innerText = random;
+};
 
-  //numeroCella.innerHTML;
-}
-numeroCella = querrySelector("div");
-//const myTable = document.querySelector("#myTable");
-//document.body.div.appendChild("myTable");
+const generateTable = function () {
+  const generateSchedule = document.getElementById("randomNumber1").value;
+  const table = document.getElementById("table2");
+  console.log(generateSchedule);
+  for (let i = 0; i < generateSchedule; i++) {
+    const outDiv = document.createElement("div");
+    outDiv.classList.add("newTable");
+    outDiv.classList.add("tablestyle");
+    for (let i = 0; i < 24; i++) {
+      const cellNumber = document.createElement("div");
+      cellNumber.classList.add("cell");
+      cellNumber.classList.add("cellColor");
+      const cellContent = document.createElement("h3");
+      cellContent.innerText = i + 1;
 
-// const bottone = document.querySelector("button");
-// bottone = Math.random(0 , 76);
-// console.log(bottone)
+      cellNumber.appendChild(cellContent);
+      outDiv.appendChild(cellNumber);
+    }
+    table.appendChild(outDiv);
+  }
+};
+
